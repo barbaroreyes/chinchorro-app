9d69d148c7cf6638a7b6454b8f4bb130c9aed4b2
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-07-29 12:46:58 -0500

    added-amplify

---
+++ src/graphql/queries.js
@@ -0,0 +1,101 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getOrder = /* GraphQL */ `
+  query GetOrder($id: ID!) {
+    getOrder(id: $id) {
+      id
+      user
+      date
+      total
+      plates {
+        items {
+          id
+          plate_id
+          order_id
+          createdAt
+          updatedAt
+          customer
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      customer
+    }
+  }
+`;
+export const listOrders = /* GraphQL */ `
+  query ListOrders(
+    $filter: ModelOrderFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listOrders(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        user
+        date
+        total
+        plates {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        customer
+      }
+      nextToken
+    }
+  }
+`;
+export const getPlate = /* GraphQL */ `
+  query GetPlate($id: ID!) {
+    getPlate(id: $id) {
+      id
+      name
+      description
+      image
+      category
+      featured
+      price
+      orders {
+        items {
+          id
+          plate_id
+          order_id
+          createdAt
+          updatedAt
+          customer
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listPlates = /* GraphQL */ `
+  query ListPlates(
+    $filter: ModelPlateFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPlates(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        image
+        category
+        featured
+        price
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
