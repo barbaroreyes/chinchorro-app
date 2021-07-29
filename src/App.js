e5e385f60d77a470676becae12429d20a7056190
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-07-29 14:48:37 -0500

    fixing

--- src/App.js
+++ src/App.js
@@ -1,55 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// Amplify
-import Amplify from "aws-amplify";
-
-// Pages
-import Home from "./pages/Home"
-import Error from "./pages/Error";
-import Books from "./pages/Plates";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import PlatesDetails from "./pages/PlatesDetails";
-import Admin from './pages/Admin';
-
-// Components
-import Header from "./components/Header"
-
-// Amplify Configurations
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
-
-
+import React from 'react'
+import Admin from './pages/Admin'
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/cart">
-          <Cart />
-        </Route>
-        <Route path="/checkout">
-          <Checkout />
-        </Route>
-        <Route exact path="/plates">
-          <Books />
-        </Route>
-        <Route
-          path="/plates/:id"
-          children={<PlatesDetails></PlatesDetails>}>
-        </Route>
-        <Route path="/admin">
-          <Admin />
-        </Route>
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    <div>
+      <Admin/>
+    </div>
+  )
 }
 
-export default App;
+export default App
+
